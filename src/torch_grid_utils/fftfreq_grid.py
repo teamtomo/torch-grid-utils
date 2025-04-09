95e21890bcbe09341ab0651bbabda934f9cd4348
alisterburt <alisterburt@gmail.com>
2025-04-09 13:54:34 -0400

    expose dft_center and rfft_shape at top level (#10)
    * expose dft_center and rfft_shape APIs at top level of package and declare __all__
    
    * import shapes at top level

--- src/torch_grid_utils/fftfreq_grid.py
+++ src/torch_grid_utils/fftfreq_grid.py
@@ -165,7 +165,7 @@ def rfft_shape(input_shape: Sequence[int]) -> tuple[int]:
 def dft_center(
     image_shape: tuple[int, ...],
     rfft: bool,
-    fftshifted: bool,
+    fftshift: bool,
     device: torch.device | None = None,
 ) -> torch.LongTensor:
     """Return the position of the DFT center for a given input shape."""
@@ -173,7 +173,7 @@ def dft_center(
     image_shape = torch.as_tensor(image_shape).float()
     if rfft is True:
         image_shape = torch.tensor(rfft_shape(image_shape))
-    if fftshifted is True:
+    if fftshift is True:
         fft_center = torch.divide(image_shape, 2, rounding_mode='floor')
     if rfft is True:
         fft_center[-1] = 0
