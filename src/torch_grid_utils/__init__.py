31ffd43661c1411f739ed3a481d0fe0ecebae00f
Josh Dickerson <jdickerson@mrc-lmb.cam.ac.uk>
2025-12-28 16:50:46 -0800

    feat: add patch grid (#24)

--- src/torch_grid_utils/__init__.py
+++ src/torch_grid_utils/__init__.py
@@ -1,4 +1,4 @@
-"""Grids for 2D/3D image manipulations in PyTorch"""
+"""Grids for 2D/3D image manipulations in PyTorch."""
 
 from importlib.metadata import PackageNotFoundError, version
 
@@ -11,20 +11,26 @@ __email__ = "alisterburt@gmail.com"
 
 from torch_grid_utils.coordinate_grid import coordinate_grid, image_center
 from torch_grid_utils.fftfreq_grid import (
-    fftfreq_grid,
     dft_center,
-    rfft_shape,
-    fftshift_1d,
-    ifftshift_1d,
-    fftshift_2d,
-    ifftshift_2d,
-    fftshift_3d,
-    ifftshift_3d,
+    fftfreq_grid,
     fftfreq_to_spatial_frequency,
+    fftshift_1d,
+    fftshift_2d,
+    fftshift_3d,
+    ifftshift_1d,
+    ifftshift_2d,
+    ifftshift_3d,
+    rfft_shape,
     spatial_frequency_to_fftfreq,
 )
+from torch_grid_utils.patch_grid import (
+    patch_grid,
+    patch_grid_centers,
+    patch_grid_indices,
+    patch_grid_lazy,
+)
 from torch_grid_utils.shapes_2d import circle, rectangle, square, wedge
-from torch_grid_utils.shapes_3d import sphere, cuboid, cube, cone
+from torch_grid_utils.shapes_3d import cone, cube, cuboid, sphere
 
 __all__ = [
     "coordinate_grid",
@@ -48,4 +54,8 @@ __all__ = [
     "cuboid",
     "cube",
     "cone",
+    "patch_grid",
+    "patch_grid_lazy",
+    "patch_grid_centers",
+    "patch_grid_indices",
 ]
---
+++ src/torch_grid_utils/patch_grid/_patch_grid_centers.py
@@ -0,0 +1,213 @@
+"""Utilities for extracting centers of a grid of patches from an image."""
+
+from collections.abc import Sequence
+from typing import cast
+
+import einops
+import torch
+
+
+def patch_grid_centers(
+    image_shape: tuple[int, int] | tuple[int, int, int],
+    patch_shape: tuple[int, int] | tuple[int, int, int],
+    patch_step: tuple[int, int] | tuple[int, int, int],
+    distribute_patches: bool = True,
+    device: torch.device = None,
+) -> torch.Tensor:
+    """
+    Extract centers of a grid of patches from an image.
+
+    Parameters
+    ----------
+    image_shape: tuple[int, int] | tuple[int, int, int]
+        (h, w) or (d, h, w) shape of the image.
+    patch_shape: tuple[int, int] | tuple[int, int, int]
+        `(patch_h, patch_w)` or `(patch_d, patch_h, patch_w)`
+        of patches to be extracted.
+    patch_step: tuple[int, int] | tuple[int, int, int]
+        The target distance between patch centers in dimensions `h` and `w`.
+    distribute_patches: bool
+        Whether to distribute patches across the entire dimension length (`True`)
+        or leave a gap at the end of each dimension (`False`).
+    device: torch.device, optional
+        Device for computation. Default is None, which uses the device of the
+        input image.
+
+    Returns
+    -------
+    torch.Tensor
+        (..., grid_h, grid_w, 2) array of coordinates of patch centers
+        in image dimensions `h` and `w`.
+    """
+    parameters_are_valid = len(image_shape) == len(patch_shape) and len(
+        image_shape
+    ) == len(patch_step)
+    if parameters_are_valid is False:
+        raise ValueError(
+            "image shape, patch length and patch step are not the same length."
+        )
+    ndim = len(image_shape)
+    if ndim == 2:
+        return _patch_centers_2d(
+            image_shape=image_shape,
+            patch_shape=cast("tuple[int, int]", patch_shape),
+            patch_step=cast("tuple[int, int]", patch_step),
+            distribute_patches=distribute_patches,
+            device=device,
+        )
+    elif ndim == 3:
+        return _patch_centers_3d(
+            image_shape=image_shape,
+            patch_shape=cast("tuple[int, int, int]", patch_shape),
+            patch_step=cast("tuple[int, int, int]", patch_step),
+            distribute_patches=distribute_patches,
+            device=device,
+        )
+    else:
+        raise NotImplementedError("only 2D and 3D patches currently supported")
+
+
+def _patch_centers_1d(
+    dim_length: int,
+    patch_length: int,
+    patch_step: int,
+    distribute_patches: bool = True,
+    device: torch.device = None,
+) -> torch.Tensor:
+    """
+    Extract centers of a 1D grid of patches from an image.
+
+    Parameters
+    ----------
+    dim_length: int
+        Length of the dimension.
+    patch_length: int
+        Length of the patch.
+    patch_step: int
+        The target distance between patch centers.
+    distribute_patches: bool
+        Whether to distribute patches across the entire dimension length (`True`)
+        or leave a gap at the end of each dimension (`False`).
+    device: torch.device, optional
+        Device for computation. Default is None, which uses the device of the
+        input image.
+
+    Returns
+    -------
+    torch.Tensor
+        (..., patch_length) array of patch centers.
+    """
+    min_bound = patch_length // 2
+    max_bound = dim_length - min_bound - 1
+    if max_bound < min_bound:
+        max_bound = min_bound
+    patch_centers = torch.arange(
+        min_bound, max_bound + 1, step=patch_step, device=device
+    )
+    if distribute_patches is True:
+        delta = max_bound - patch_centers[-1]
+        shifts = torch.linspace(0, delta, steps=len(patch_centers), device=device)
+        patch_centers += torch.round(shifts).long()
+    return patch_centers
+
+
+def _patch_centers_2d(
+    image_shape: Sequence[int],
+    patch_shape: tuple[int, int],
+    patch_step: tuple[int, int],
+    distribute_patches: bool = True,
+    device: torch.device = None,
+) -> torch.Tensor:
+    """
+    Extract centers of a 2D grid of patches from an image.
+
+    Parameters
+    ----------
+    image_shape: Sequence[int]
+        (h, w) shape of the image.
+    patch_shape: tuple[int, int]
+        `(patch_h, patch_w)` of patches to be extracted.
+    patch_step: tuple[int, int]
+        The target distance between patch centers in dimensions `h` and `w`.
+    distribute_patches: bool
+        Whether to distribute patches across the entire dimension length (`True`)
+        or leave a gap at the end of each dimension (`False`).
+    distribute_patches: bool
+    device: torch.device, optional
+        Device for computation. Default is None, which uses the device of the
+        input image.
+
+    Returns
+    -------
+    torch.Tensor
+        (..., grid_h, grid_w, 2) array of coordinates of patch centers
+        in image dimensions `h` and `w`.
+    """
+    pc_h, pc_w = (
+        _patch_centers_1d(
+            dim_length=dim_length,
+            patch_length=patch_length,
+            patch_step=step,
+            distribute_patches=distribute_patches,
+            device=device,
+        )
+        for dim_length, patch_length, step in zip(
+            image_shape[-2:], patch_shape, patch_step
+        )
+    )
+    n_ph, n_pw = len(pc_h), len(pc_w)
+    phh = einops.repeat(pc_h, "ph -> ph pw", pw=n_pw)
+    pww = einops.repeat(pc_w, "pw -> ph pw", ph=n_ph)
+    patch_centers = einops.rearrange([phh, pww], "hw h w -> h w hw")
+    return patch_centers
+
+
+def _patch_centers_3d(
+    image_shape: Sequence[int],
+    patch_shape: tuple[int, int, int],
+    patch_step: tuple[int, int, int],
+    distribute_patches: bool = True,
+    device: torch.device = None,
+) -> torch.Tensor:
+    """
+    Extract centers of a 3D grid of patches from an image.
+
+    Parameters
+    ----------
+    image_shape: Sequence[int]
+        (d, h, w) shape of the image.
+    patch_shape: tuple[int, int, int]
+        `(patch_d, patch_h, patch_w)` of patches to be extracted.
+    patch_step: tuple[int, int, int]
+        The target distance between patch centers in dimensions `d`, `h` and `w`.
+    distribute_patches: bool
+        Whether to distribute patches across the entire dimension length (`True`)
+        or leave a gap at the end of each dimension (`False`).
+    device: torch.device, optional
+        Device for computation. Default is None, which uses the device of the
+        input image.
+
+    Returns
+    -------
+    torch.Tensor
+        (..., grid_d, grid_h, grid_w, 3) array of coordinates of patch centers
+        in image dimensions `d`, `h` and `w`.
+    """
+    pc_d, pc_h, pc_w = (
+        _patch_centers_1d(
+            dim_length=dim_length,
+            patch_length=patch_length,
+            patch_step=step,
+            distribute_patches=distribute_patches,
+            device=device,
+        )
+        for dim_length, patch_length, step in zip(
+            image_shape[-3:], patch_shape, patch_step
+        )
+    )
+    n_pd, n_ph, n_pw = len(pc_d), len(pc_h), len(pc_w)
+    pdd = einops.repeat(pc_d, "pd -> pd ph pw", ph=n_ph, pw=n_pw)
+    phh = einops.repeat(pc_h, "ph -> pd ph pw", pd=n_pd, pw=n_pw)
+    pww = einops.repeat(pc_w, "pw -> pd ph pw", pd=n_pd, ph=n_ph)
+    patch_centers = einops.rearrange([pdd, phh, pww], "dhw d h w -> d h w dhw")
+    return patch_centers
